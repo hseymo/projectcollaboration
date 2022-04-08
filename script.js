bde9f2857cefce1c4190da74a7a773e135258b69
Kalif Purce <kalifpurce@gmail.com>
2022-04-08 13:07:15 -0700

    Added basic api function

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 // super awesome efficient code goes here
+
+function getApi() { 
+
+    var requestUrl = 'https://www.loc.gov/search/?q= ';
+    fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    console.log(requestUrl);
+}
